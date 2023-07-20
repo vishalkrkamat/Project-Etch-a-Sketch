82e41106a55146d4fdbd2bd383bc3eb0a17dfc71
meanwhile7 <abhijit793001@gmail.com>
2023-07-20 17:34:24 +0530

    range function value taken

--- script/script.js
+++ script/script.js
@@ -1,30 +1,6 @@
 //creating dynamic grid
 let container = document.querySelector('#grid');
 
-// Manipulate the grid template using JavaScript
-container.style.gridTemplateRows = 'repeat(39, 1fr)'; // Set the grid template rows
-container.style.gridTemplateColumns = 'repeat(39, 1fr)'; // Set the grid template columns
-// for (let i = 0; i <= 11; i++) {
-//   let elem = document.createElement('span');
-//   elem.classList.add('grid-item');
-//   container.appendChild(elem);
-// }
-
-const gridItems = [
-  'Item 1',
-  'Item 2',
-  'Item 3',
-  'Item 4',
-  'Item 5',
-  'Item 6'
-];
-
-gridItems.forEach(item => {
-  const gridElement = document.createElement('div');
-  gridElement.classList.add('grid-item');
-  container.appendChild(gridElement);
-});
-
 const rangeInput = document.getElementById('myRange');
 const rangeValueDisplay = document.getElementById('rangeValue');
 
@@ -34,6 +10,7 @@ rangeInput.addEventListener('input', function() {
   rangeValueDisplay.textContent = selectedValue;
   const rangePercent = (selectedValue - rangeInput.min) / (rangeInput.max - rangeInput.min) * 100;
   rangeInput.style.backgroundSize = rangePercent + '% 100%';
+  creategrid(selectedValue); // Call creategrid() with the selectedValue as an argument
 });
 
 rangeInput.addEventListener('mousemove', function(e) {
@@ -41,4 +18,9 @@ rangeInput.addEventListener('mousemove', function(e) {
   rangeValueDisplay.textContent = selectedValue;
   rangeValueDisplay.style.left = (-60+e.clientX) + 'px';
 });
+function creategrid(temv){
+  container.style.gridTemplateRows = `repeat(${temv}, 1fr)`; // Set the grid template rows
+  container.style.gridTemplateColumns = `repeat(${temv}, 1fr)` // Set the grid template columns
+}
+
 
